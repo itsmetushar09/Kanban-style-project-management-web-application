b394b00d603758c080fcc4dc6fba0095ceb9245d
itsmetushar09 <itsmetushar307@gmail.com>
2026-03-29 16:44:55 +0530

    kk

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://kanban-style-project-management-web-tqrg.onrender.com/api/boards",
+  baseURL: "https://kanban-style-project-management-web-tqrg.onrender.com/api",
 });
 
 export default API;
